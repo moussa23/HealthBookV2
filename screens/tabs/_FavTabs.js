1e6825595a301c6d54a6ab3afb14e3d8ace760f1
moussa23 <mosawi.man@live.fr>
2020-07-19 20:48:26 +0100

    tab styles adeed

--- screens/tabs/_FavTabs.js
+++ screens/tabs/_FavTabs.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 
 //screens
 import Doctores from './doctors';
@@ -7,16 +7,74 @@ import Ambulance from './ambulance';
 import Labs from './labs';
 import Urgent from './urgent';
 
+//icons for tabs
+import { Fontisto,FontAwesome5 } from '@expo/vector-icons';
+
 const tabs = createMaterialTopTabNavigator();
 
 export default class _FavTabs extends Component {
+
+    getTabLabel(tab) {
+        switch (tab) {
+            case 'Doctores':
+                return 'Doctores';
+            case 'Ambulance':
+                return 'Ambulances';
+            case 'Labs':
+                return 'Laboratoires';
+            case 'Urgences':
+                return 'Urgences';
+        }
+    }
+
     render() {
         return (
-            <tabs.Navigator>
-                <tabs.Screen name='Doctores' component={Doctores}/>
-                <tabs.Screen name='Ambulance' component={Ambulance}/>
-                <tabs.Screen name='Labs' component={Labs}/>
-                <tabs.Screen name='Urgences' component={Urgent}/>
+            <tabs.Navigator
+                
+                screenOptions={({ route }) =>
+                    ({
+                        
+                        tabBarIcon: ({ focused }) => {
+                            let color;
+                            focused ? color = '#ff7539' : color = '#28696d'
+                            switch (route.name) {
+                                case 'Doctores':
+                                    return <Fontisto name="doctor" size={25} color={color} />
+                                case 'Ambulance':
+                                    return <FontAwesome5 name="ambulance" size={21} color={color} />
+                                case 'Labs':
+                                    return <Fontisto name="laboratory" size={24} color={color} />
+                                case 'Urgences':
+                                    return <FontAwesome5 name="hospital-alt" size={24} color={color} />
+                            }
+                        },
+
+                        title: this.getTabLabel(route.name)
+
+
+                    })
+                }
+                tabBarOptions={
+                    {
+                        
+                        pressColor: '#55efc4',
+                        indicatorStyle: { backgroundColor: '#ff7539' },
+                        labelStyle: {
+                            textTransform: 'capitalize',
+                            width: 100
+                        },
+                        showIcon: true,
+                        iconStyle:{
+                            width:50,
+                            alignItems:'center'
+                        }
+                    }
+                }
+            >
+                <tabs.Screen name='Doctores' component={Doctores} />
+                <tabs.Screen name='Ambulance' component={Ambulance} />
+                <tabs.Screen name='Labs' component={Labs} />
+                <tabs.Screen name='Urgences' component={Urgent} />
             </tabs.Navigator>
         )
     }
--- screens/tabs/_homeTabs.js
+++ screens/tabs/_homeTabs.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 
 //tabs
 import Appoint from './appoint';
@@ -7,21 +7,77 @@ import Analysis from './analysis';
 import Radiology from './radiology';
 import Surgey from './surgey';
 
+import {
+    MaterialCommunityIcons,
+    FontAwesome,
+    Fontisto,
+    SimpleLineIcons,
+    FontAwesome5,
+    Entypo
+} from '@expo/vector-icons';
 
 const tabs = createMaterialTopTabNavigator();
 
 export default class _homeTabs extends Component {
+    getTabLabel(tab) {
+        switch (tab) {
+            case 'appoint':
+                return 'Rendez-vous';
+            case 'analy':
+                return 'Analyses';
+            case 'radio':
+                return 'Radiologie';
+            case 'suregey':
+                return 'Chirurgie';
+        }
+    }
     render() {
         return (
-      
-            <tabs.Navigator>
-                <tabs.Screen name='appoint'  component={Appoint} />
-                <tabs.Screen name='analy'  component={Analysis} />
-                <tabs.Screen name='radio'  component={Radiology} />
-                <tabs.Screen name='suregey'  component={Surgey} />
+            <tabs.Navigator
+                initialRouteName='appoint'
+                swipeEnabled={true}
+                screenOptions={({ route }) =>
+                    ({
+                        tabBarIcon: ({ focused }) => {
+                            let color
+                            focused ? color = '#ff7539' : color = '#28696d'
+                            switch (route.name) {
+                                case 'appoint':
+                                    return <FontAwesome name='calendar' size={25} color={color} />
+                                case 'analy':
+                                    return <Fontisto name="test-tube" size={24} color={color} />
+                                case 'radio':
+                                    return <FontAwesome5 name="x-ray" size={20} color={color} />
+                                case 'suregey':
+                                    return <Fontisto name="surgical-knife" size={24} color={color} />
+                            }
+                        },
+
+                        title: this.getTabLabel(route.name)
+
+
+                    })
+                }
+                tabBarOptions={
+                    {
+                        pressColor:'#55efc4',
+                        indicatorStyle: { backgroundColor: '#ff7539' },
+                        labelStyle: {
+                            textTransform: 'capitalize',
+                            width: 100
+                        },
+                        showIcon: true,
+
+                    }
+                }
+            >
+                <tabs.Screen name='appoint' component={Appoint} />
+                <tabs.Screen name='analy' component={Analysis} />
+                <tabs.Screen name='radio' component={Radiology} />
+                <tabs.Screen name='suregey' component={Surgey} />
             </tabs.Navigator>
-        
+
         );
-        
+
     }
 }
