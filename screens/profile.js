a6a9a553a7fc8757b54ffff185e7be03ca0d2ccd
AyaaG <ayaomrana@gmail.com>
2020-07-25 21:49:38 +0100

    PROFILE[

--- screens/profile.js
+++ screens/profile.js
@@ -1,13 +1,51 @@
 import React, { Component } from 'react'
 import { Text, StyleSheet, View } from 'react-native'
+import FieldSet from 'react-native-fieldset'; 
 
 export default class profile extends Component {
     render() {
+       
         return (
             <View style={styles.container} >
-                <Text> Profile page </Text>
+            <View> <FieldSet label="Profile basique"> <Text>Nom Prenom : </Text>
+           <Text>Telephone: </Text>
+           <Text>Date de naissance: </Text> 
+           <Text>Genre: </Text>
+           <Text>Email: </Text>
+           </FieldSet> </View> 
             </View>
-        )
+            <View> <FieldSet label="Profie etendu"> 
+            <AppButton
+                            height={40}
+                            onPress={() => this.anteced()}
+                            loading={this.state.loading}
+                            title='Antecedents medicaux'
+                            width='60%'
+                        />
+                        <AppButton
+                            height={40}
+                            onPress={() => this.famil()}
+                            loading={this.state.loading}
+                            title='Antecedents familiaux'
+                            width='60%'
+                        />
+                        <AppButton
+                            height={40}
+                            onPress={() => this.habitudes()}
+                            loading={this.state.loading}
+                            title='Habitudes alcolo-tabagiques'
+                            width='60%'
+                        />
+                        <AppButton
+                            height={40}
+                            onPress={() => this.chirur()}
+                            loading={this.state.loading}
+                            title='Chirurgie'
+                            width='60%'
+                        />
+         </FieldSet></View>
+         </View>
+        );
     }
 }
 
