291f41a6333fab9ab81500f6647665a883b5e3a2
moussa23 <mosawi.man@live.fr>
2020-07-25 22:50:29 +0100

    fixed

--- screens/profile.js
+++ screens/profile.js
@@ -1,58 +1,62 @@
 import React, { Component } from 'react'
 import { Text, StyleSheet, View } from 'react-native'
-import FieldSet from 'react-native-fieldset'; 
+import FieldSet from 'react-native-fieldset';
 
 export default class profile extends Component {
     render() {
-       
+
         return (
+
             <View style={styles.container} >
-            <View> <FieldSet label="Profile basique"> <Text>Nom Prenom : </Text>
-           <Text>Telephone: </Text>
-           <Text>Date de naissance: </Text> 
-           <Text>Genre: </Text>
-           <Text>Email: </Text>
-           </FieldSet> </View> 
+                <View>
+                    <FieldSet label="Profile basique"> <Text>Nom Prenom : </Text>
+                        <Text>Telephone: </Text>
+                        <Text>Date de naissance: </Text>
+                        <Text>Genre: </Text>
+                        <Text>Email: </Text>
+                    </FieldSet> 
+                 </View>
+            
+            <View> <FieldSet label="Profie etendu">
+                <AppButton
+                    height={40}
+                    onPress={() => this.anteced()}
+                    loading={this.state.loading}
+                    title='Antecedents medicaux'
+                    width='60%'
+                />
+                <AppButton
+                    height={40}
+                    onPress={() => this.famil()}
+                    loading={this.state.loading}
+                    title='Antecedents familiaux'
+                    width='60%'
+                />
+                <AppButton
+                    height={40}
+                    onPress={() => this.habitudes()}
+                    loading={this.state.loading}
+                    title='Habitudes alcolo-tabagiques'
+                    width='60%'
+                />
+                <AppButton
+                    height={40}
+                    onPress={() => this.chirur()}
+                    loading={this.state.loading}
+                    title='Chirurgie'
+                    width='60%'
+                />
+            </FieldSet>
             </View>
-            <View> <FieldSet label="Profie etendu"> 
-            <AppButton
-                            height={40}
-                            onPress={() => this.anteced()}
-                            loading={this.state.loading}
-                            title='Antecedents medicaux'
-                            width='60%'
-                        />
-                        <AppButton
-                            height={40}
-                            onPress={() => this.famil()}
-                            loading={this.state.loading}
-                            title='Antecedents familiaux'
-                            width='60%'
-                        />
-                        <AppButton
-                            height={40}
-                            onPress={() => this.habitudes()}
-                            loading={this.state.loading}
-                            title='Habitudes alcolo-tabagiques'
-                            width='60%'
-                        />
-                        <AppButton
-                            height={40}
-                            onPress={() => this.chirur()}
-                            loading={this.state.loading}
-                            title='Chirurgie'
-                            width='60%'
-                        />
-         </FieldSet></View>
-         </View>
-        );
+         </View >
+        )
     }
 }
 
 const styles = StyleSheet.create({
-    container:{
-        flex:1,
-        alignItems:'center'
+    container: {
+        flex: 1,
+        alignItems: 'center'
     }
 
 })
--- screens/signUp.js
+++ screens/signUp.js
@@ -1,28 +1,27 @@
 import React, { Component } from 'react'
 import { Text, StyleSheet, View, Image, Alert, ScrollView } from 'react-native';
 import { Ionicons, FontAwesome } from '@expo/vector-icons';
-import {Switch,TextInput,Button} from 'react-native-paper';
+import { Switch, TextInput, RadioButton } from 'react-native-paper';
 import logo from '../assets/logo.png';
 import { Entypo } from '@expo/vector-icons';
 import AppInput from '../components/textInput';
 import AppButton from '../components/button';
-import { RadioGroup, RadioButton } from 'react-radio-buttons';
 export default class signUp extends Component {
     constructor(props) {
         super(props);
         this.state = {
-   
+
             checked: 'first',
             mail: '',
             password: '',
-            nom:'',
-            prenom:'',
-            date:'',
-            pays:'',
-            ville:'',
-            confirmation:'',
-            masculin:'',
-            feminim:'',
+            nom: '',
+            prenom: '',
+            date: '',
+            pays: '',
+            ville: '',
+            confirmation: '',
+            masculin: '',
+            feminim: '',
             showPass: true,
         }
     }
@@ -30,128 +29,151 @@ export default class signUp extends Component {
     render() {
         const { checked } = this.state;
 
-            return ( <ScrollView>
-           
-                    <View style={styles.container}>
-                        <View style={{ alignItems: 'center' }}>
-                            <Image style={styles.logo} source={logo} resizeMode='contain' />
-                        </View>
-                        
-                            <Text style={{ fontSize: 16, marginTop: 10 }} >
-                            <FontAwesome style={{ paddingLeft: 10 }} name='user' size={38} color='#2869cd' /> Nom :
-                            </Text>
-                         
-                            <TextInput
-                            
-             style={styles.input}
-             placeholder=''
-             autoCapitalize='none'
-             underlineColorAndroid='transparent'
-             onChangeText={(nom) => this.setState({nom})}
-             value={this.state.nom}
-             
-         />
-           
-        
-         <Text style={{ fontSize: 16, marginTop: 10 }} >
-             <FontAwesome style={{ paddingLeft: 10 }} name='user' size={38} color='#2869cd' /> Prenom : </Text>
-                            <TextInput
-             style={styles.input}
-             placeholder=''
-             autoCapitalize='none'
-             underlineColorAndroid='transparent'
-             onChangeText={(prenom) => this.setState({prenom})}
-             value={this.state.prenom}
-         />
-          <Text style={{ fontSize: 16, marginTop: 10 }} >
-              <FontAwesome style={{ paddingLeft: 10 }} name='calendar' size={38} color='#2869cd' /> Date de naissance : </Text>
-                            <TextInput
-             style={styles.input}
-             placeholder=''
-             autoCapitalize='none'
-             underlineColorAndroid='transparent'
-             onChangeText={(date) => this.setState({date})}
-             value={this.state.date}
-         />
-        <Text style={{ fontSize: 16, marginTop: 10 }} >
-              <FontAwesome style={{ paddingLeft: 10 }} name='flag' size={38} color='#2869cd' /> Pays : </Text>
-                            <TextInput
-             style={styles.input}
-             placeholder=''
-             autoCapitalize='none'
-             underlineColorAndroid='transparent'
-             onChangeText={(pays) => this.setState({pays})}
-             value={this.state.pays}
-         />
-          <Text style={{ fontSize: 16, marginTop: 10 }} >
-              <FontAwesome style={{ paddingLeft: 10 }} name='map-marker' size={38} color='#2869cd' /> Ville : </Text>
-                            <TextInput
-             style={styles.input}
-             placeholder=''
-             autoCapitalize='none'
-             underlineColorAndroid='transparent'
-             onChangeText={(ville) => this.setState({ville})}
-             value={this.state.ville}
-         />
-         <Text style={{ fontSize: 16, marginTop: 10 }} >
-              <FontAwesome style={{ paddingLeft: 10 }} name='envelope-open' size={38} color='#2869cd' /> Email : </Text>
-                            <TextInput
-             style={styles.input}
-             placeholder=''
-             autoCapitalize='none'
-             underlineColorAndroid='transparent'
-             onChangeText={(email) => this.setState({email})}
-             value={this.state.email}
-         />
-         <Text style={{ fontSize: 16, marginTop: 10 }} >
-              <FontAwesome style={{ paddingLeft: 10 }} name='lock' size={38} color='#2869cd' /> Mot de passe : </Text>
-                            <TextInput
-             style={styles.input}
-             placeholder=''
-             autoCapitalize='none'
-             underlineColorAndroid='transparent'
-             onChangeText={(password) => this.setState({password})}
-             value={this.state.password}
-         />
-         <Text style={{ fontSize: 16, marginTop: 10 }} >
-               Confirmation : </Text>
-                            <TextInput
-             style={styles.input}
-             placeholder=''
-             autoCapitalize='none'
-             underlineColorAndroid='transparent'
-             onChangeText={(confirmation) => this.setState({confirmation})}
-             value={this.state.confirmation}
-         />
- <View>
-        <RadioGroup
-          onValueChange={(v) => {this.setState({checked: v})}}
-        >
-        <RadioButton
-          value="Masculin"
-          status={checked === 'Masculin' ? 'checked' : 'unchecked'}
-          onPress={() => { this.setState({ checked: 'Masculin' }); }}
-        />
-        <RadioButton
-          value="Feminin"
-          status={checked === 'Feminin' ? 'checked' : 'unchecked'}
-          onPress={() => { this.setState({ checked: 'Feminin' }); }}
-        />
-        </RadioGroup>
-      </View>
+        return (
+            <ScrollView>
+                <View style={styles.container}>
+                    <View style={{ alignItems: 'center' }}>
+                        <Image style={styles.logo} source={logo} resizeMode='contain' />
+                    </View>
 
-         <AppButton
-                            height={40}
-                            onPress={() => this.loginUser()}
-                            loading={this.state.loading}
-                            title='Enregistrer'
-                            width='60%'
-                        />
-         
-                   
+                    <Text style={{ fontSize: 16, marginTop: 10 }} >
+                        <FontAwesome style={{ paddingLeft: 10 }} name='user' size={38} color='#00695c' /> Nom :
+                     </Text>
+                    <TextInput
+                        theme={{
+                            colors: { primary: '#ff7539', background: 'rgba(0,0,0,0.1)', placeholder: 'rgba(0,0,0,0.3)' },
+                            animation: { scale: 2 },
+                        }}
+                        style={styles.input}
+                        label='Nom'
+                        autoCapitalize='none'
+                        underlineColorAndroid='transparent'
+                        onChangeText={(nom) => this.setState({ nom })}
+                        value={this.state.nom}
+                    />
+
+                    <Text style={{ fontSize: 16, marginTop: 10 }} >
+                        <FontAwesome style={{ paddingLeft: 10 }} name='user' size={38} color='#00695c' /> Prenom : </Text>
+                    <TextInput
+                        theme={{
+                            colors: { primary: '#ff7539', background: 'rgba(0,0,0,0.1)', placeholder: 'rgba(0,0,0,0.3)' },
+                            animation: { scale: 2 },
+                        }}
+                        label='Prénom'
+                        style={styles.input}
+                        placeholder=''
+                        autoCapitalize='none'
+                        underlineColorAndroid='transparent'
+                        onChangeText={(prenom) => this.setState({ prenom })}
+                        value={this.state.prenom}
+                    />
+                    <Text style={{ fontSize: 16, marginTop: 10 }} >
+                        <FontAwesome style={{ paddingLeft: 10 }} name='calendar' size={38} color='#00695c' /> Date de naissance : </Text>
+                    <TextInput
+                        theme={{
+                            colors: { primary: '#ff7539', background: 'rgba(0,0,0,0.1)', placeholder: 'rgba(0,0,0,0.3)' },
+                            animation: { scale: 2 },
+                        }}
+                        label='date de naissance'
+                        style={styles.input}
+                        placeholder=''
+                        autoCapitalize='none'
+                        underlineColorAndroid='transparent'
+                        onChangeText={(date) => this.setState({ date })}
+                        value={this.state.date}
+                    />
+
+                    <Text style={{ fontSize: 16, marginTop: 10 }} >
+                        <FontAwesome style={{ paddingLeft: 10 }} name='phone' size={38} color='#00695c' /> Téléphone  : </Text>
+                    <TextInput
+                        theme={{
+                            colors: { primary: '#ff7539', background: 'rgba(0,0,0,0.1)', placeholder: 'rgba(0,0,0,0.3)' },
+                            animation: { scale: 2 },
+                        }}
+                        label='Téléphone'
+                        style={styles.input}
+                        placeholder=''
+                        autoCapitalize='none'
+                        underlineColorAndroid='transparent'
+                        onChangeText={(ville) => this.setState({ ville })}
+                        value={this.state.ville}
+                    />
+                    <Text style={{ fontSize: 16, marginTop: 10 }} >
+                        <FontAwesome style={{ paddingLeft: 10 }} name='envelope-open' size={38} color='#00695c' /> Email : </Text>
+                    <TextInput
+                        theme={{
+                            colors: { primary: '#ff7539', background: 'rgba(0,0,0,0.1)', placeholder: 'rgba(0,0,0,0.3)' },
+                            animation: { scale: 2 },
+                        }}
+                        label='Email'
+                        style={styles.input}
+                        placeholder=''
+                        autoCapitalize='none'
+                        underlineColorAndroid='transparent'
+                        onChangeText={(email) => this.setState({ email })}
+                        value={this.state.email}
+                    />
+                    <Text style={{ fontSize: 16, marginTop: 10 }} >
+                        <FontAwesome style={{ paddingLeft: 10 }} name='lock' size={38} color='#00695c' /> Mot de passe : </Text>
+                    <TextInput
+                        theme={{
+                            colors: { primary: '#ff7539', background: 'rgba(0,0,0,0.1)', placeholder: 'rgba(0,0,0,0.3)' },
+                            animation: { scale: 2 },
+                        }}
+                        label='Mot de passs'
+                        style={styles.input}
+                        placeholder=''
+                        autoCapitalize='none'
+                        underlineColorAndroid='transparent'
+                        onChangeText={(password) => this.setState({ password })}
+                        value={this.state.password}
+                    />
+                    <Text style={{ fontSize: 16, marginTop: 10 }} >
+                        <FontAwesome style={{ paddingLeft: 10 }} name='lock' size={38} color='#00695c'/> Confirmation de mot de pass : </Text>
+                    <TextInput
+                        theme={{
+                            colors: { primary: '#ff7539', background: 'rgba(0,0,0,0.1)', placeholder: 'rgba(0,0,0,0.3)' },
+                            animation: { scale: 2 },
+                        }}
+                        style={styles.input}
+                        placeholder=''
+                        autoCapitalize='none'
+                        underlineColorAndroid='transparent'
+                        onChangeText={(confirmation) => this.setState({ confirmation })}
+                        value={this.state.confirmation}
+                    />
+
+                    <View style={{ flexDirection: 'row',alignItems:'center'}} >
+                        <RadioButton.Group
+                            onValueChange={(v) => { this.setState({ checked: v }) }}>
+                           
+                            <RadioButton
+                                theme={{ colors: { accent: '#ff7539' } }}
+                                value="Masculin"
+                                status={checked === 'Masculin' ? 'checked' : 'unchecked'}
+                                onPress={() => { this.setState({ checked: 'Masculin' }); }}
+                            /><Text style={styles.genderText} > Masculin </Text>
+                            <RadioButton
+                                theme={{ colors: { accent: '#ff7539' } }}
+                                value="Feminin"
+                                status={checked === 'Feminin' ? 'checked' : 'unchecked'}
+                                onPress={() => { this.setState({ checked: 'Feminin' }); }}
+                            /><Text style={styles.genderText} > Feminin </Text>
+                        </RadioButton.Group>
+                    </View>
+
+                    <AppButton
+                        height={40}
+                        onPress={() => this.loginUser()}
+                        loading={this.state.loading}
+                        title='Enregistrer'
+                        width='60%'
+                    />
+
+
                 </View>
-                </ScrollView>
-        
+            </ScrollView>
+
         )
     }
 }
@@ -163,7 +185,6 @@ const styles = StyleSheet.create({
     },
     logo: {
         marginTop: '10%',
-
     },
     icon: {
         position: 'absolute',
@@ -174,6 +195,10 @@ const styles = StyleSheet.create({
         position: 'absolute',
         right: '5%',
         top: '50%'
+    },
+    genderText:{
+        fontSize:18,
+
     }
 })
 
